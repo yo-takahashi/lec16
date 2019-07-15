866abf8eec26ee44859e36c3e5bc26ff62da7403
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2019-07-15 16:17:37 +0900

    init spring-boot project.

---
+++ src/main/java/com/example/simplereminder/SimpleReminderApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplereminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleReminderApplication.class, args);
+	}
+
+}
