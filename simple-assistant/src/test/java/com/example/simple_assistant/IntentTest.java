0ca4cb58f895e43496e1df24b06387296224ab9a
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2019-07-22 03:46:47 +0900

    リマインダのメッセージを訂正し、用件に文字数制限を実施する

--- simple-assistant/src/test/java/com/example/simple_assistant/IntentTest.java
+++ simple-assistant/src/test/java/com/example/simple_assistant/IntentTest.java
@@ -21,10 +21,17 @@ public class IntentTest {
     assertEquals(Intent.REMINDER, actual);
   }
 
-  @DisplayName("Intentが〈10時20分に〉授業でREMAINDERを返す")
+  @DisplayName("Intentが〈10時20分に〉でREMAINDERを返す")
   @Test
   void makeIntentReturnsUNKNOWN02() {
     var actual = Intent.makeIntent("10時20分に");
+    assertEquals(Intent.UNKNOWN, actual);
+  }
+
+  @DisplayName("Intentは用件が32文字以上でえあればUNKNOWNを返す")
+  @Test
+  void makeIntentReturnsUNKNOWN03() {
+    var actual = Intent.makeIntent("10時20分に1234567890123456789012345678901234567890");
     assertEquals(Intent.UNKNOWN, actual);
   }
 }
