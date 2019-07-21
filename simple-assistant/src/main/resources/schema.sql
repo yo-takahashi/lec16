50005773d105a96fb441df5267957c60c52553af
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2019-07-22 05:51:31 +0900

    userId長さを調整

--- simple-assistant/src/main/resources/schema.sql
+++ simple-assistant/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table if not exists reminder_item (
   id bigserial primary key,
-  user_id varchar(32) not null,
+  user_id varchar(64) not null,
   push_At time not null,
   push_text varchar(32) not null
 );
